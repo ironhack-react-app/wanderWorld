819f0e217938363bd345a46621604a7edecd1ea8
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-02-16 14:37:01 +0100

    display picture and title in DestinationDetails

--- src/Components/DestinationDetails.jsx
+++ src/Components/DestinationDetails.jsx
@@ -6,25 +6,34 @@ const DestinationDetails = () => {
 
     const API_URL = "https://react-app-json-server-backend.adaptable.app"
 
-    const {destinationId} = useParams()
+    const { destinationId } = useParams()
 
-    const {destination, setDestination} = useState()
+    const [destination, setDestination] = useState(null)
 
     useEffect(() => {
-        axios.get(API_URL + "/destinations" + destinationId)
-        .then((response) => {
-            setDestination(response.data)
-        })
-        .catch((e) => {
-            console.log(e)
-        })
+        axios.get(API_URL + "/destinations/" + destinationId)
+            .then((response) => {
+                setDestination(response.data)
+            })
+            .catch((e) => {
+                console.log(e)
+            })
     }, [])
 
     return (
         <div className="DestinationDetails">
-            
+            {destination === null
+                ? <p>Loading...</p>
+                : (<div className="destination">
+                    <div className="img-card">
+                        <img src={destination.image} />
+                    </div>
+                    
+                    <h1>{destination.destination}</h1>
+                </div>)
+            }
         </div>
     );
 }
- 
+
 export default DestinationDetails;
