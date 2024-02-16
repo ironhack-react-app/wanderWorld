35b90717ed854948f55e8e791627dde89f4938dc
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-02-16 17:12:48 +0100

    implemented new information on the Details Page

--- src/Components/DestinationDetails.jsx
+++ src/Components/DestinationDetails.jsx
@@ -28,8 +28,32 @@ const DestinationDetails = () => {
                     <div className="img-card">
                         <img src={destination.image} />
                     </div>
-                    
-                    <h1>{destination.destination}</h1>
+                    <div className="info-wrap">
+                        <div className="info-heading">
+                            <h1>{destination.destination}</h1>
+                            <p><i>{destination.description}</i></p>
+                        </div>
+                        <div className="info-data">
+                            <div className="statistics">
+                                <h4>Warmest Month</h4>
+                                <div><p>🔥 test</p></div>
+                                <h4>Rainiest Month</h4>
+                                <div><p>🌧️ test</p></div>
+                                <h4>Cheapest Month</h4>
+                                <div><p>💸 test</p></div>
+                                <h4>Best Season to go:</h4>
+                                <div><p>😊 test</p></div>
+                            </div>
+                            <div className="price">
+                                <h4>Price 7-Days</h4>
+                                <h1>250€</h1>
+                            </div>
+                        </div>
+                        <div className="buttons-wrap">
+                        <button>Edit Destination</button>
+                        <button>Delete Destination</button>
+                        </div>
+                    </div>
                 </div>)
             }
         </div>
