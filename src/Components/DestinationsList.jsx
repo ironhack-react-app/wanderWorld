129b58fd61cd7a74b80e1ddca94e44d6956f894b
Senay Asikoglu <senayasikoglu@gmail.com>
2024-02-16 14:47:45 +0100

    add css to destinationList

--- src/Components/DestinationsList.jsx
+++ src/Components/DestinationsList.jsx
@@ -1,46 +1,64 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+//import { FaTrashCan } from "react-icons/fa6";
 
 const DestinationsList = () => {
+  const API_URL = "https://react-app-json-server-backend.adaptable.app";
 
-    const API_URL = "https://react-app-json-server-backend.adaptable.app"
+  const [destinations, setDestinations] = useState(null);
 
-    const [destinations, setDestinations] = useState(null)
+  useEffect(() => {
+    axios
+      .get(API_URL + "/destinations")
+      .then((response) => {
+        setDestinations(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(API_URL + "/destinations")
-            .then((response) => {
-                setDestinations(response.data)
-            })
-            .catch((e) => {
-                console.log(e)
-            })
-    }, [])
+  /*const deleteProduct = (destinationId) => {
+    const newList = destinations.filter((destinationObj) => {
+      return destinationObj.id !== destinationId;
+    });
+    setDestinations(newList);
+  };
+*/
+  return (
+    <div className="DestinationsList">
+      {destinations === null ? (
+        <p>Loading...</p>
+      ) : (
+        destinations.map((obj) => {
+          return (
+            <div className="destination-card" key={obj.id}>
+              <Link to={`/destinations/${obj.id}`}>
+                <h2 className="destination-title">{obj.destination}</h2>
 
-    return (
-        <div className="DestinationsList">
-            {destinations === null
-                ? <p>Loading...</p>
-                : destinations.map((obj) => {
-                    return (
-                        <div className="destination-card" key={obj.id}>
-                        <Link to={`/destinations/${obj.id}`}>
-                           
-                            <h2 className="destination-title">{obj.destination}</h2>
-                           
-                            <img className="item-img" src={obj.image}/>
-                            <p className="destination-description">{obj.description}</p>
-                            
-                            
-                       </Link>
-                       </div>
-                    )
-                })
-            }
+                <img className="item-img" src={obj.image} />
+                <p className="destination-description">{obj.description}</p>
 
-        </div>
-    );
-}
+              </Link>
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
+};
 
 export default DestinationsList;
+/*
+                <div className="btn-container">
+                  <button
+                    className="trash"
+                    onClick={() => {
+                      deleteProduct(obj.id);
+                    }}
+                  >
+                    <FaTrashCan />
+                  </button>
+                </div>
+                */
